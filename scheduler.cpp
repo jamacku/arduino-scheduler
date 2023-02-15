69782f527b88d1d25387343a2f1e905137dfe9b4
Jan Macku <jamacku@redhat.com>
2023-02-15 20:08:45 +0100

    WIP: Scheduler

---
+++ scheduler.cpp
@@ -0,0 +1,11 @@
+/*
+    blink.cpp - Simple example in creating your own Arduino Library
+    Copyright (c) op from TMM. All right reserved.
+*/
+ 
+#include <Arduino.h>
+#include "scheduler.hpp"
+
+Scheduler::Scheduler(){
+    // TODO ...
+}
---
+++ scheduler.hpp
@@ -0,0 +1,16 @@
+/*
+    TODO ...
+*/
+
+#ifndef Scheduler_h
+#define Scheduler_h
+
+#include <Arduino.h>
+
+class Scheduler {
+    public:
+        Scheduler();
+        // TODO ...
+};
+
+#endif
